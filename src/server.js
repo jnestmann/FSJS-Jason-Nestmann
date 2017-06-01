a0ec63e2f862d95f3aa4ecdc55bdc8eff7968968
Jason Nestmann <nestmannj@bellsouth.net>
2017-06-01 10:40:15 -0400

    Added config, router and bodyParser to server file

--- src/server.js
+++ src/server.js
@@ -2,22 +2,24 @@
 
 const express = require('express');
 const pug = require('pug');
-const jsonParser = require('body-parser').json;
+const jsonParser = require('body-parser');
+
+const config = require('./config');
+const router = require('./routes');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use('/static/', express.static("./public"));
+app.use(jsonParser.json());
+
+// sends all rountes to be handled by the router.js file
+app.use('/', router);
+
 
 app.set('view engine', 'pug');
 app.set('views', './views');
 
 app.listen(port, function(){
-    console.log(`The express server is running on port ${port}`);
-});
-
-
-// routes
-app.get('/', function(req, res) {
-    res.render('index');
+    console.log(`${config.app_name} is running on port ${config.port}`);
 });
