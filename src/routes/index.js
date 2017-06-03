97a43a018bec945b424b71fcdda5162098f0a6de
Jason Nestmann <nestmannj@bellsouth.net>
2017-06-03 13:01:55 -0400

    Added a page to display list of games in db

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,47 +1,62 @@
 // src/routes/index.js
 const router = require('express').Router();
-const fs = require('fs');
+const jsonParser = require('body-parser');
 
 const data_api = require('../game_data_api.js');
 
 
 router.get('/', function(req, res, next){
     res.render('index');
-})
-
-router.use('/doc', function(req, res, next) {
-  res.end(`Documentation http://expressjs.com/`);
-});
-
-router.get('/file', function(req, res, next) {
-  res.json(data_api.get_all());
-});
-
-
-router.post('/file', function(req, res, next) {
-    data_api.post(req, res);
-    next();
-});
-            
-
-router.put('/file/:fileId', function(req, res, next) {
-    data_api.put(req, res);
     next();
 });
 
-router.delete('/file/:fileId', function(req, res, next) {
-    data_api.delete(req, res);
-    next();
+router.post('/add', function(req, res, next){
+    console.log("Post request made");
+    console.log(req.body);
+    res.render('add');
 });
 
-router.get('/file/:fileId', function(req, res, next) {
-    data_api.get(req, res);
-    next();
+router.get('/add', function(req, res, next){
+    res.render('add');
 });
 
-router.use('/file/save', function(req, res, next){
-    data_api.save(req, res);
-    next();
-})
+router.get('/add/file', function(req, res, next){
+    data_api.post2(req.query);
+    res.render('add');
+});
+
+router.post('/add/file', function(req, res, next){
+    console.log("Post request");
+    data_api.post2(req.body);
+    res.render('add');    
+});
+
+//router.get('/file/search', function(req, res, next) {
+//    res.render('search');
+//    next();
+//});
+
+router.get('/files', function(req, res, next) {
+    const games = data_api.get_all();
+    console.log(games);
+    res.render('games_list', {games: games});
+    //res.json(data_api.get_all());
+});
+
+
+//router.put('/file/:fileId', function(req, res, next) {
+//    data_api.put(req, res);
+//    next();
+//});
+
+//router.delete('/file/:fileId', function(req, res, next) {
+//    data_api.delete(req, res);
+//    next();
+//});
+
+//router.get('/file/:fileId', function(req, res, next) {
+//    data_api.get(req, res);
+//    next();
+//});
 
 module.exports = router;
